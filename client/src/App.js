ea09dfac4a1e706f51636699f4d0b83bf7d26e1b
Wiktor Łukaszenko <iktorn2006@wp.pl>
2022-10-30 22:27:22 +0100

    tr

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import Detction from './Components/Detection/Detection';
 import './App.css';
 
 function App() {
-    return (
-        <div className="container">
-            <Detction />
-        </div>
-    );
+	return (
+		<div className="container">
+			<Detction />
+		</div>
+	);
 }
 
 export default App;
+
+/* 
+	- Dark mode
+
+*/
